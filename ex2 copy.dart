10019f9ecf11f092f3e72a4518b706f56967b840
Kelvin <kelvinsilvadev@gmail.com>
2020-09-24 07:43:56 -0300

    excluido_ultima_linha

--- ex2 copy.dart
+++ ex2 copy.dart
@@ -6,5 +6,5 @@ void main() {
     if (altura == 150<175){ print("Média");}
     if (altura < 150){print("Baixa");}
     if (altura ==175<198){print("Alta");}
-    if (altura >198){print("Muito Alta");}
+    
 }
