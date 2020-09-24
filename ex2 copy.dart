21aca8628d353afbeea833bab44e6c4323fdec25
Kelvin <kelvinsilvadev@gmail.com>
2020-09-24 07:41:48 -0300

    criado_backup

---
+++ ex2 copy.dart
@@ -0,0 +1,10 @@
+void main() {
+
+    int altura = 150;
+
+    // ifs aqui
+    if (altura == 150<175){ print("Média");}
+    if (altura < 150){print("Baixa");}
+    if (altura ==175<198){print("Alta");}
+    if (altura >198){print("Muito Alta");}
+}
